1d76ff04c198ec42f8225f2339fda6a47b0a7583
Alireza <2144764+akrami@users.noreply.github.com>
2023-11-08 14:38:39 +0100

    kube readme

--- kube-deployment/nfs.sh
+++
@@ -1,2 +0,0 @@
-helm repo add nfs-subdir-external-provisioner <https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/>
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=192.168.178.203 --set nfs.path=/storage --create-namespace --namespace nfs
